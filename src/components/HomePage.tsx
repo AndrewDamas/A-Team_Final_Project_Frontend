f4774b64c37d93992ccc390eaeaab9cd54541671
Andrew Damavoletes <damas.andrew@gmail.com>
2022-07-07 19:34:09 -0400

    HomePage done

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,15 @@
+import { Link, useNavigate } from "react-router-dom";
+import "../styles/HomePage.css"
+import pokemonLogo from "../images/pokemon-logo.png";
+import pokemonVersionLogo from "../images/pokemon-version-logo.png";
+
+export default function HomePage(){
+    const navigate = useNavigate();
+    return (
+        <div className="HomePage" onClick={() => {navigate("/")}}>
+            <img className="pokemon-logo" src={pokemonLogo} alt="Pokemon" />
+            <img className="pokemon-version-logo" src={pokemonVersionLogo} alt="Pokemon Javascript Version" />
+            <p className="click-anywhere-to-start">Click anywhere to start</p>
+        </div>
+    )
+}
