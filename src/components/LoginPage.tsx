84d414106e4653bebfa34f56b94adddf0f83708b
akatko <97916468+akatko@users.noreply.github.com>
2022-07-07 20:53:39 -0400

    login page CSS

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
   return (
     <div className="LoginPage">
         <form action="">
-            <h1>Log In</h1>
+            <h1 className="LoginHeader">Log In</h1>
             <label htmlFor="username"></label>
             <input type="text" name="username" id="username" placeholder="Enter Username Here" />
             <label htmlFor="password"></label>
