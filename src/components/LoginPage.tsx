57531885968cdee4101b85feba78e6b22e01ad5f
akatko <97916468+akatko@users.noreply.github.com>
2022-07-11 19:42:54 -0400

    fix login css

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -3,6 +3,7 @@ import "../styles/LoginPage.css"
 import { Link, useNavigate } from "react-router-dom";
 
 export default function LoginPage() {
+  const navigate = useNavigate();
   return (
     <div className="LoginPage">
         <form action="">
@@ -11,7 +12,7 @@ export default function LoginPage() {
             <input type="text" name="username" id="username" placeholder="Enter Username Here" />
             <label htmlFor="password"></label>
             <input type="password" name="password" id="password" placeholder="Enter Password Here" />
-            <input className="loginSubmit" type="submit" value="Log In" />
+            <input className="loginSubmit" type="submit" value="Log In" onClick={() => {navigate("/welcome")}} />
             <a className="createAccount" href="">Create Account</a>
         </form>
     </div>
