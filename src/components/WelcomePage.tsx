64f180e17835c8bf1d8a1c1b7bb7cd878e368e1c
akatko <97916468+akatko@users.noreply.github.com>
2022-07-11 21:03:04 -0400

    added slides and font

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../styles/WelcomePage.css"
 import professorOak from "../images/professor-oak2.webp";
 
 export default function WelcomePage() {
+   const [slides, setSlides] = useState(1)
   return (
     <div className="WelcomePage">
         <img className="professor-oak" src={professorOak} alt="Professor Oak"/>
+        <div>
+            {
+                slides === 1 ?
+                <div>
+                <p>Hello there! Welcome to the world of POKÉMON! My name is OAK! People call me the POKÉMON PROF!</p>
+                <button className="SlideButton" onClick={() => setSlides(slides +1)}>Next</button>
+                </div> :
+                slides === 2 ?
+                <p>This world is inhabited by creatures called POKÉMON! For some people, POKÉMON are pets. Others use them for fights. Myself...I study POKÉMON as a profession.</p>: 
+                <p></p>
+            }
+        </div>
     </div>
   )
 }
