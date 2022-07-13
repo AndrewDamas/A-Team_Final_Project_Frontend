4b83bf9e97129191e7001937c4f41fe845d9d620
akatko <97916468+akatko@users.noreply.github.com>
2022-07-13 17:55:19 -0400

    Welcome page media query + textbox

--- src/components/WelcomePage.tsx
+++ src/components/WelcomePage.tsx
@@ -1,24 +1,50 @@
-import React, { useState } from 'react';
-import "../styles/WelcomePage.css"
+import React, { useState } from "react";
+import "../styles/WelcomePage.css";
 import professorOak from "../images/professor-oak2.webp";
 
 export default function WelcomePage() {
-   const [slides, setSlides] = useState(1)
+  const [slides, setSlides] = useState(1);
   return (
     <div className="WelcomePage">
-        <img className="professor-oak" src={professorOak} alt="Professor Oak"/>
-        <div>
-            {
-                slides === 1 ?
-                <div>
-                <p>Hello there! Welcome to the world of POKÉMON! My name is OAK! People call me the POKÉMON PROF!</p>
-                <button className="SlideButton" onClick={() => setSlides(slides +1)}>Next</button>
-                </div> :
-                slides === 2 ?
-                <p>This world is inhabited by creatures called POKÉMON! For some people, POKÉMON are pets. Others use them for fights. Myself...I study POKÉMON as a profession.</p>: 
-                <p></p>
-            }
-        </div>
+      <img className="professor-oak" src={professorOak} alt="Professor Oak" />
+      <div>
+        {slides === 1 ? (
+          <div className="Container">
+            <div className="TextBox">
+              <p>
+                Hello there! Welcome to the world of POKÉMON! My name is OAK!
+                People call me the POKÉMON PROF!
+              </p>
+            </div>
+            <div className="SlideButton">
+            <button
+              onClick={() => setSlides(slides + 1)}
+            >
+              Next
+            </button>
+            </div>
+          </div>
+        ) : slides === 2 ? (
+        <div className="Container">
+        <div className="TextBox">
+          <p>
+            This world is inhabited by creatures called POKÉMON! For some
+            people, POKÉMON are pets. Others use them for fights. Myself...I
+            study POKÉMON as a profession.
+          </p>
+          </div>
+          <div className="SlideButton">
+            <button
+              onClick={() => setSlides(slides + 1)}
+            >
+              Next
+            </button>
+            </div>
+          </div>
+        ) : (
+          <p></p>
+        )}
+      </div>
     </div>
-  )
+  );
 }
